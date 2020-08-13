7914c71fd3e3b82758dc6fa93f8e765cd0d8bb26
Vasha Faris <vashafaris@gmail.com>
2020-08-13 22:24:24 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import ProductList from './components/ProductList';
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedProducts: [],
+      products: [
+        { id: 1, name: 'AirMax 90', brand: 'Nike' },
+        { id: 2, name: 'Yeezy', brand: 'Adidas' },
+        { id: 3, name: 'Classic', brand: 'Reebok' },
+      ],
+    };
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  handleProductSelect(product) {
+    this.setState((prevState) => {
+      return {
+        selectedProducts: prevState.selectedProducts.concat(product),
+      };
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>My Product Store</h1>
+        <ProductList
+          products={this.state.products}
+          onProductSelect={this.handleProductSelect.bind(this)}
+        />
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/components/ProductList.test.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import Adapter from 'enzyme-adapter-react-16';
+import { shallow, configure } from 'enzyme';
+import ProductList from './ProductList';
+
+configure({ adapter: new Adapter() });
+
+let mockProducts, wrapper, productSelectFn;
+beforeEach(() => {
+  // This is run before every test
+  mockProducts = [
+    { id: 1, name: 'Mock Product 1', brand: 'MockBrandA' },
+    { id: 2, name: 'Mock Product 2', brand: 'MockBrandB' },
+    { id: 3, name: 'Mock Product 3', brand: 'MockBrandC' },
+  ];
+
+  productSelectFn = jest.fn();
+
+  // const productSelectFn = (productSelected) =>
+  //   console.log('You selected', productSelected);
+
+  wrapper = shallow(
+    <ProductList products={mockProducts} onProductSelect={productSelectFn} />
+  );
+});
+
+afterEach(() => {
+  productSelectFn.mockReset();
+});
+
+it('should render an <li> element for every product in `props.products`', () => {
+  expect(wrapper.find('li').length).toEqual(mockProducts.length);
+});
+it('should display the product name in each `<li>` element', () => {
+  const firstElement = wrapper.find('li').first();
+  expect(firstElement.contains(mockProducts[0].name)).toEqual(true);
+});
+it('should display the brand name in each `<li>` element', () => {
+  const firstElement = wrapper.find('li').first();
+  expect(firstElement.contains(mockProducts[0].brand)).toEqual(true);
+});
+it('should call `props.onProductSelect` when an <li> is clicked', () => {
+  const firstElement = wrapper.find('li').first();
+  // We check that the function has not been called yet
+  expect(productSelectFn.mock.calls.length).toEqual(0);
+  // We click the <li>
+  firstElement.simulate('click');
+  // We check that the function has now been called
+  expect(productSelectFn.mock.calls.length).toEqual(1);
+  // We check it's been called with the right arguments
+  expect(productSelectFn.mock.calls[0][0]).toEqual(mockProducts[0]);
+});
